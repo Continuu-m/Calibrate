3eb6358bfc0f2502c99ec2f0a478e31eec44ab9e
N1CK999 <noble.nick05@gmail.com>
2026-02-14 15:20:54 +0530

    Task model plus fastapi init

---
+++ backend/models/task.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column,Integer,String
+from backend.db.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
